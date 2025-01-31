5b52294d433057c2e43221bd0bebdbb323b07339
macioszczyk.mikolaj@gmail.com <macioszczyk.mikolaj@gmail.com>
2025-01-31 17:05:46 +0100

    httpClient base

--- Orders/Application/Services/HttpBasketService.cs
+++ Orders/Application/Services/HttpBasketService.cs
@@ -1,48 +1,14 @@
 ﻿using Application.Interfaces;
 using Application.Models;
-using System.Net;
-using System.Text.Json.Serialization;
-using System.Text.Json;
+using Common.Application.Services;
 
 namespace Application.Services;
 
-public class HttpBasketService : IBasketService
+public class HttpBasketService : HttpServiceBase, IBasketService
 {
-    private readonly HttpClient _httpClient;
+    public HttpBasketService(HttpClient httpClient) : base(httpClient)
+    {}
 
-    public HttpBasketService(HttpClient httpClient)
-    {
-        _httpClient = httpClient;
-    }
-
-    public async Task<Basket?> GetBasketById(Guid basketId)
-    {
-        var productResponse = await _httpClient.GetAsync($"Basket/{basketId}");
-
-        if (productResponse.IsSuccessStatusCode)
-        {
-            var responseContent = await productResponse.Content.ReadAsStringAsync();
-            return JsonSerializer.Deserialize<Basket>(responseContent, SerializerOptions.Value);
-        }
-        else if (productResponse.StatusCode == HttpStatusCode.NotFound)
-        {
-            return null;
-        }
-
-        throw new HttpRequestException($"Request failed with status code {productResponse.StatusCode}");
-    }
-
-    // TODO: Base HttpClient
-    private static readonly Lazy<JsonSerializerOptions> SerializerOptions = new(InitializeSerializerOptions());
-
-    private static JsonSerializerOptions InitializeSerializerOptions()
-    {
-        var options = new JsonSerializerOptions
-        {
-            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-            PropertyNameCaseInsensitive = true,
-        };
-        options.Converters.Add(new JsonStringEnumConverter());
-        return options;
-    }
+    public Task<Basket?> GetBasketById(Guid basketId)
+        => Get<Basket>($"Basket/{basketId}");
 }
