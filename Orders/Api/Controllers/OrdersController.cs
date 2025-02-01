8e3b39838315e21ba4031bd34fcc9d0e469675ae
macioszczyk.mikolaj@gmail.com <macioszczyk.mikolaj@gmail.com>
2025-02-01 13:59:55 +0100

    estimated consts

--- Orders/Api/Controllers/OrdersController.cs
+++ Orders/Api/Controllers/OrdersController.cs
@@ -1,5 +1,6 @@
 using Application.Requests.Orders;
 using Application.Requests.Orders.CreateOrder;
+using Application.Requests.Orders.GetExpectedOrderCost;
 using Application.Requests.Orders.GetOrderById;
 using Application.Requests.Orders.GetOrderHistory;
 using Common.Api.Controllers;
@@ -19,6 +20,11 @@ namespace Api.Controllers
         [AllowAnonymous]
         [HttpGet("{Id}")]
         public Task<ActionResult<OrderResult>> GetOrderById([FromRoute] GetOrderByIdQuery query) =>
+            HandleRequest(query);
+
+        [AllowAnonymous]
+        [HttpGet("costs")]
+        public Task<ActionResult<GetExpectedOrderCostQueryResult>> GetExpectedOrderCost([FromQuery] GetExpectedOrderCostQuery query) =>
             HandleRequest(query);
 
         [Authorize(Roles = RoleNames.Customer)]
