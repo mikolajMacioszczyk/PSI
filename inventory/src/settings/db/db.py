74fc42df3c58c7d04574ad7b459bfa0c79a6dfa4
EC2_BUILD <adam.stypczyc@gmail.com>
2025-02-01 04:06:55 +0100

    Docker v1 added

--- inventory/src/settings/db/db.py
+++ inventory/src/settings/db/db.py
@@ -1,7 +1,11 @@
-DB_USER = "postgres"
-DB_PASSWORD = "221001"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "warehouse"
+import os
+
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "221001")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+if os.getenv("DOCKER_ENV"):
+    DB_HOST = "postgres"
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "warehouse")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
