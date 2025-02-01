3b6751e1644f4108a9c9e97705e25627075a5b5d
macioszczyk.mikolaj@gmail.com <macioszczyk.mikolaj@gmail.com>
2025-02-01 18:56:22 +0100

    updated script

--- performanceTests/run.sh
+++ performanceTests/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup python3 -m locust -f ./chat_requests.py --headless -u 50 -r 5 --csv stats/chat_requests --run-time 5h > /dev/null 2>&1 &
+nohup python3 -m locust -f ./basket_requests.py --headless -u 50 -r 5 --csv stats/basket_requests --run-time 5h > /dev/null 2>&1 &
