7c2c02b4bb7944b2e7d0b213b2f7043e9152459f
EC2_BUILD <adam.stypczyc@gmail.com>
2025-02-02 20:04:31 +0100

    Websocket and cors fixed

--- inventory/src/entrypoints/http/__init__.py
+++ inventory/src/entrypoints/http/__init__.py
@@ -1,4 +1,3 @@
 from .product_routes import get_products, update_product_stock, get_product, get_all_products, create_product
-from .websocket_routes import websocket_endpoint
 from .notification_routes import create_notification, get_notifications, get_notification_by_id
 from .warehouse_routes import get_all_warehouses
