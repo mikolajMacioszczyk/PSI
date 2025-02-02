88228fde87b3badb0d2485d13c67d80982b593dd
macioszczyk.mikolaj@gmail.com <macioszczyk.mikolaj@gmail.com>
2025-02-02 12:45:52 +0100

    inventory health check

--- inventory/src/entrypoints/http/__init__.py
+++ inventory/src/entrypoints/http/__init__.py
@@ -2,3 +2,4 @@ from .product_routes import get_products, update_product_stock, get_product, get
 from .websocket_routes import websocket_endpoint
 from .notification_routes import create_notification, get_notifications, get_notification_by_id
 from .warehouse_routes import get_all_warehouses
+from .health_routes import health_check
